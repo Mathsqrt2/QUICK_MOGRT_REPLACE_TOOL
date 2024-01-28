935d2d63b5422e214a07e8529ec046cd56104daf
Mathsqrt2 <micho.bugajski@gmail.com>
2024-01-28 23:46:07 +0100

    new heading style, title correction, and manifest fix

--- jsx/extendScript.jsx
+++ jsx/extendScript.jsx
@@ -1,17 +1,12 @@
 $.mogrts_control = {
     checkMode: function(){
-
     },
     checkSelectedItems: function(){
-
     },
     displayAllElementsProperties: function(){
-        alert("display All");
     },
     displaySelectedItemsProperties: function(){
-        alert("display selected");
     },
     processReplacement: function(){
-        
     }
 }
