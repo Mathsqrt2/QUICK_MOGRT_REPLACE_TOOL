a71df7d3d50e2d4837b444e7b66c2009eb2f4a33
Mathsqrt2 <micho.bugajski@gmail.com>
2024-01-28 23:06:01 +0100

    first shape of new plugin

--- jsx/extendScript.jsx
+++ jsx/extendScript.jsx
@@ -1,4 +1,16 @@
-$.replaceMogrts = {
+$.mogrts_control = {
+    checkMode: function(){
+
+    },
+    checkSelectedItems: function(){
+
+    },
+    displayAllElementsProperties: function(){
+        alert("display All");
+    },
+    displaySelectedItemsProperties: function(){
+        alert("display selected");
+    },
     processReplacement: function(){
         
     }
