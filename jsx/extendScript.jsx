a4324b0df47c2e0080d3130cd4fa8ae0bb6e5d80
Mathsqrt2 <micho.bugajski@gmail.com>
2024-01-29 23:12:41 +0100

    finding elements button added

--- jsx/extendScript.jsx
+++ jsx/extendScript.jsx
@@ -1,9 +1,28 @@
+var proj = app.project;
+var seq = proj.activeSequence;
+
 $.mogrts_control = {
+
+    userConfigHandling: function(){
+    },
     checkMode: function(){
     },
     checkSelectedItems: function(){
     },
     displayAllElementsProperties: function(){
+    var mogrts = [], counterMGT = 0;
+        for(i = 0; i < proj.activeSequence.videoTracks.length; i++){
+            for(j = 0; j < seq.videoTracks[i].clips.length; j++){
+                seq.videoTracks[i].clips[j];
+                if(seq.videoTracks[i].clips[j].isMGT()){
+                    mogrts.push(seq.videoTracks[i].clips[j]);
+                    counterMGT++;
+                }
+                
+            }
+        }
+    
+        return mogrts;
     },
     displaySelectedItemsProperties: function(){
     },
