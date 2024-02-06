b1b8fdc3bb6f0d38eee0c9b06ae49c2f630b9795
Mathsqrt2 <micho.bugajski@gmail.com>
2024-02-06 03:53:24 +0100

    small fix in mgt function

--- jsx/extendScript.jsx
+++ jsx/extendScript.jsx
@@ -12,29 +12,37 @@ $.mogrts_control = {
     displayAllElementsProperties: function(){
         var allMgrtElements = [];
 
-        if(seq.videoTracks.length > 0){
+        if(seq.videoTracks.length){
             var clipsCounter = 0;
             for(var i = 0; i < seq.videoTracks.length; i++){
-                if(seq.videoTracks[i].clips.length > 0){
+                if(seq.videoTracks[i].clips.length){
                     clipsCounter++;
                     for(var j = 0; j < seq.videoTracks[i].clips.length; j++){
                         if(seq.videoTracks[i].clips[j].isMGT()){
-                            var outputObject = {
-                                name: seq.videoTracks[i].clips[j].name,
-                                properties: [],
-                                instances: [],
-                            }
                             if(!this.namePresenceCheck(allMgrtElements,seq.videoTracks[i].clips[j].name)){
+                                var outputObject = {
+                                    name: seq.videoTracks[i].clips[j].name,
+                                    properties: [],
+                                    instances: [],
+                                }
+                                outputObject.instances.push(seq.videoTracks[i].clips[j]);
+                                for(var l = 0; l < seq.videoTracks[i].clips[j].components.length; l++){
+                                    if(seq.videoTracks[i].clips[j].components[l].displayName == "Graphic Parameters"){
+                                        for(var m = 0; m < seq.videoTracks[i].clips[j].components[l].properties.length; m++){
+                                            var propertyObject = {
+                                                name: seq.videoTracks[i].clips[j].components[l].properties[m].displayName,
+                                                value: seq.videoTracks[i].clips[j].components[l].properties[m].getValue(),
+                                            };
+                                            outputObject.properties.push(propertyObject);
+                                        }
+                                    }
+                                }
                                 allMgrtElements.push(outputObject);
-                                
+                            } else {
                                 for(var k = 0; k < allMgrtElements.length; k++){
                                     if(allMgrtElements[k].name == seq.videoTracks[i].clips[j].name){
                                         allMgrtElements[k].instances.push(seq.videoTracks[i].clips[j]);
                                     }
-                                }
-                            } else {
-                                for(var k = 0; k < allMgrtElements.length; k++){
-                                    allMgrtElements[k].instances.push(seq.videoTracks[i].clips[j]);
                                 }
                             }                            
                         }
@@ -47,10 +55,8 @@ $.mogrts_control = {
         } else {
             alert("this sequence doesn't contain any video tracks!");
         }
-
         this.saveLogs(config,"displayAllElements");
-        alert("number of found MGT" + JSON.stringify(allMgrtElements));
-        return allMgrtElements;
+        return JSON.stringify(allMgrtElements);
     },
 
     processReplacement: function(){
@@ -93,6 +99,7 @@ $.mogrts_control = {
 
         return fixedPath;
     },
+
     namePresenceCheck: function(array,element){
         for(var i = 0; i < array.length; i++){
             if(array[i].name == element){
