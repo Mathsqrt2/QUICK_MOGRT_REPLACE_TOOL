712561410c5c8ac80cdd175879d9aa80710f7a66
Mathsqrt2 <micho.bugajski@gmail.com>
2024-02-06 02:38:47 +0100

    finding all MGT instances works

--- jsx/extendScript.jsx
+++ jsx/extendScript.jsx
@@ -10,9 +10,47 @@ function loadConfig(userConfig){
 
 $.mogrts_control = {
     displayAllElementsProperties: function(){
-        
+        var allMgrtElements = [];
+
+        if(seq.videoTracks.length > 0){
+            var clipsCounter = 0;
+            for(var i = 0; i < seq.videoTracks.length; i++){
+                if(seq.videoTracks[i].clips.length > 0){
+                    clipsCounter++;
+                    for(var j = 0; j < seq.videoTracks[i].clips.length; j++){
+                        if(seq.videoTracks[i].clips[j].isMGT()){
+                            var outputObject = {
+                                name: seq.videoTracks[i].clips[j].name,
+                                properties: [],
+                                instances: [],
+                            }
+                            if(!this.namePresenceCheck(allMgrtElements,seq.videoTracks[i].clips[j].name)){
+                                allMgrtElements.push(outputObject);
+                                
+                                for(var k = 0; k < allMgrtElements.length; k++){
+                                    if(allMgrtElements[k].name == seq.videoTracks[i].clips[j].name){
+                                        allMgrtElements[k].instances.push(seq.videoTracks[i].clips[j]);
+                                    }
+                                }
+                            } else {
+                                for(var k = 0; k < allMgrtElements.length; k++){
+                                    allMgrtElements[k].instances.push(seq.videoTracks[i].clips[j]);
+                                }
+                            }                            
+                        }
+                    }
+                } 
+            }
+            if(clipsCounter < 1){
+                alert("this sequence doesn't contain any clips!");
+            }
+        } else {
+            alert("this sequence doesn't contain any video tracks!");
+        }
 
         this.saveLogs(config,"displayAllElements");
+        alert("number of found MGT" + JSON.stringify(allMgrtElements));
+        return allMgrtElements;
     },
 
     processReplacement: function(){
@@ -55,9 +93,9 @@ $.mogrts_control = {
 
         return fixedPath;
     },
-    presenceCheck: function(array,element){
-        for(var i = 0; i <= array.length; i++){
-            if(array[i] == element){
+    namePresenceCheck: function(array,element){
+        for(var i = 0; i < array.length; i++){
+            if(array[i].name == element){
                 return 1;
             }
         }
