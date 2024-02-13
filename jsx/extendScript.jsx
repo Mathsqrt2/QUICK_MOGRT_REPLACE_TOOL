d3653aa629fe8dca894b1c257440fba347935200
Mathsqrt2 <micho.bugajski@gmail.com>
2024-02-13 19:47:30 +0100

    first launch detection added, then use guide will open in default browser

--- jsx/extendScript.jsx
+++ jsx/extendScript.jsx
@@ -9,6 +9,29 @@ function loadConfig(userConfig) {
     pluginPath = $.mogrts_control.fixPath(config.presetPath);
 }
 
+function isItFirstUseJSX(path) {
+    var newResponse = {
+        isItFirstUse: true,
+        actionTime: null,
+    };
+
+    var logPath = $.runScript.fixPath(path) + $.runScript.fixPath("\\logs\\firstLaunchLog.json");
+    var firstLaunchLog = new File(logPath);
+
+    if (firstLaunchLog.exists) {
+        newResponse.isItFirstUse = false;
+    } else {
+        var currentTime = new Date();
+        newResponse.actionTime = currentTime.getTime();
+        firstLaunchLog.open("w");
+        firstLaunchLog.write(JSON.stringify(newResponse));
+        firstLaunchLog.close();
+    }
+
+    var output = JSON.stringify(newResponse);
+    return output;
+}
+
 $.mogrts_control = {
     displayAllElementsProperties: function() {
         var allMgrtElements = [];
